8efcb2d5a8a5f324a9b2b1be6b863fc3af454095
optimizia-agents <optimizia.agents@gmail.com>
2026-09-12 19:44:31 +0200

    Eliminar selector de ordenación antiguo

--- app.js
+++ app.js
@@ -600,6 +600,5 @@ separateCenterFilters();
 const renderCenterStable=renderCenter;
 renderCenter=function(){const selected=$('center-category')?.value||'all';renderCenterStable();const select=$('center-category');if(select&&[...select.options].some(option=>option.value===selected))select.value=selected};
 document.querySelector('#center-category')?.addEventListener('change',()=>renderCenter());
-document.querySelector('#center-sort option[value="activity"]')?.remove();
-document.querySelector('#center-sort option[value="frequency"]')?.remove();
+document.querySelector('#center-sort')?.remove();
 const refreshCenterMetricsOriginal=refreshKURROMetrics;refreshKURROMetrics=function(){refreshCenterMetricsOriginal();renderSimpleCenterSummary()};
